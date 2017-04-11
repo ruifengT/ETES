e7ccc0106a35fbe96b89e0ff7cf1e7b951d9b387
JThai <admin@AZ-MacBook-Pro.local>
2017-04-11 11:31:15 -0700

    updated Ticket Listing.php

--- php/TicketListing.php
+++ php/TicketListing.php
@@ -1,11 +1,7 @@
 <?php
 require "Config.php";
-echo "Ticket Listing<br>";
-
-$ticket_id = $_POST['ticket_id'];
 
 $sql = "SELECT ticket_name, ticket_detail, ticket_quantity, ticket_price, ticket_pickup_address, ticket_stmp FROM tickets";
-
 $result = mysqli_query($conn, $sql);
 
 while($row = mysqli_fetch_array($result)){
