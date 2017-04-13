88d00f789854453b0eed61156310fd522f70a46c
JThai <admin@AZ-MacBook-Pro.local>
2017-04-13 16:37:35 -0700

    Just needs a bit of tweeking for displaying tickets

---
+++ ShoppingCart/testTicket.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> ETES - Online Shopping Cart </title>
+<meta charset="utf-8" />
+<link rel="stylesheet" href="css/style.css" media="screen" type="text/css" />
+</head>
+<body>
+
+<div id="site">
+	<header id="masthead">
+		<h1>ETES<span class="tagline">Online Ticket Marketplace </h1>
+	</header>
+	<div id="content">
+		<div id="products">
+		<ul>
+		<?php
+		require "../php/Config.php";
+
+		$sql = "SELECT ticket_id, ticket_name, ticket_detail, ticket_quantity, ticket_price, ticket_pickup_address FROM tickets";
+		$result = mysqli_query($conn, $sql);
+
+		while ($row = mysqli_fetch_assoc($result)){
+			$ticket_id = $row['ticket_id'];
+			$ticket_name = $row['ticket_name'];
+			$ticket_detail = $row['ticket_detail'];
+			$ticket_quantity = $row['ticket_quantity'];
+			$ticket_price = $row['ticket_price'];
+			$ticket_pickup_address = $row['ticket_pickup_address'];
+
+			?> 
+			<!-- 	break then parse information -->
+			<!-- HTML Information To Populate Blobs-->
+			<li>
+				<div class="product-description" data-name=$ticket_name data-price=$ticket_price>
+					<h4 class="product-name"> <?php echo $ticket_name?> </h4>
+					<h5> <?php echo $ticket_detail?> </h5>
+					<h6> <?php echo $ticket_pickup_address?> </h6>
+					<p class="product-price">&dollar;<?php echo $ticket_price?></p>
+					<form class="add-to-cart" action="cart.html" method="post">
+						<div>
+						<label for="qty-1">Quantity</label>
+						<input type="text" name="qty-1" id="qty-1" class="qty" value="1" />
+						</div>
+						<p><input type="submit" value="Add to cart" class="btn"></p>
+					</form>
+				</div>
+			</li>
+			<?php
+			} // ending while loop
+			?>
+
+			</ul>
+		</div>
+	</div>
+</div>
+</body>
+</html>
