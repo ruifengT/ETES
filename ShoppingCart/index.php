8f96ad1d520ea2be6920bba4b1dfedb0160b8b77
ruifengT <ruifeng.tian@sjsu.edu>
2017-04-19 20:52:14 -0700

    Revert "fix bugs and errors"
    This reverts commit 6acc955b68bb967da619820eed18fb15a3ef4346.

--- ShoppingCart/index.php
+++ ShoppingCart/index.php
@@ -27,9 +27,8 @@
 		<?php
 		require "../php/Config.php";
 
-		if(!empty($_GET['user_id'])){
-            $id = $_GET['user_id'];
-        }
+		// getting current user's id
+		$id = $_GET['user_id'];
 
         $sql = "SELECT distinct ticket_id, ticket_name, ticket_detail, ticket_quantity, ticket_price, ticket_pickup_address FROM tickets";
         if(!empty($_GET['search'])){
@@ -55,7 +54,7 @@
 					<h5> <?php echo $ticket_detail?> </h5>
 					<h6> <?php echo $ticket_pickup_address?> </h6>
 					<p class="product-price">&dollar;<?php echo $ticket_price?></p>
-					<form class="add-to-cart" action="cart.html?ticket_id=<?php echo $ticket_id ?>&user_id=<?php echo $id ?>" method="post">
+					<form class="add-to-cart" action="cart.html?ticket_id=<?php echo $ticket_id ?>?currentUser=<?php echo $id ?>" method="post">
 					<!-- Remove Echo from formClass for user ID -->
 						<div>
 						<label for="qty-1">Quantity</label>
--- php/Orders.php
+++ php/Orders.php
@@ -1,9 +1,9 @@
 <?php
 require "Config.php";
 
-$user_id = $_POST['user_id'];
-$ticket_id = $_POST['ticket_id'];
-$ticket_quantity = $_POST['ticket_quantity'];
+$user_id = '0000'; // change to the buyer's user id
+$ticket_id = 1; // change to the ticket's id
+$ticket_quantity = 5; // change to how many tickets bought
 $datetime = date('YmdHis');
 
 $sql_price = "SELECT `ticket_price` from `tickets` where `ticket_id` = " . $ticket_id;
