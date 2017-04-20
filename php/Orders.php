e59c47c5b9ba98542b807133c92eaadf5cbec7d1
ruifengT <ruifeng.tian@sjsu.edu>
2017-04-19 20:29:18 -0700

    Making orders

--- php/Orders.php
+++ php/Orders.php
@@ -18,11 +18,10 @@ $ticket_total_price = $data[0][0] * $ticket_quantity;
 $sql_insert_order = "INSERT INTO `orders`(`order_buy_user_id`, `order_sell_user_id`, `order_ticket_id`, `order_ticket_quantity`, `order_price`, `order_stmp`) VALUES ('" . $user_id . "',(SELECT `ticket_postedby` FROM `tickets` WHERE `ticket_id` = " . $ticket_id . ")," . $ticket_id . "," . $ticket_quantity . "," . $ticket_total_price . ", '" . $datetime . "')";
 
 if(mysqli_query($conn, $sql_insert_order)){
-    echo "Success!"; // change the outcome for error handling
+    echo "Order palced in database.";
 }
 else{
-    echo "Fail."; // also change here if you want to
+    echo "Fail to place order in database.";
 }
-
 mysqli_close($conn);
 ?>
