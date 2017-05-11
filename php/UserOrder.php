ae19ffee6e93216ba3bcb2819fc222ba542423ec
ruifengT <ruifeng.tian@sjsu.edu>
2017-05-10 23:32:30 -0700

    display order with ticket name & details

--- php/UserOrder.php
+++ php/UserOrder.php
@@ -3,7 +3,7 @@ require "Config.php";
 
 $user_id = $_POST['user_id'];
 
-$sql = "SELECT * FROM `orders` WHERE `order_buy_user_id` = '" . $user_id . "' or `order_sell_user_id` = '" . $user_id . "'";
+$sql = "SELECT * FROM `orders` join `tickets` on `orders`.`order_ticket_id` = `tickets`.`ticket_id` WHERE `order_buy_user_id` = '" . $user_id . "' or `order_sell_user_id` = '" . $user_id . "'";
 
 $result = mysqli_query($conn, $sql);
 while($row = mysqli_fetch_array($result)){
